a8f941e60270b255788cd3f7a9961d8dfcccfe00
Baha Sorkun <114817265+bahasorkun@users.noreply.github.com>
2024-12-03 17:36:35 +0300

    Update home.dart
    -"home.dart" : Yeni todo ekleme textfieldi olusturuldu ve + butonu eklendi

--- todo_list_app/lib/screens/home.dart
+++ todo_list_app/lib/screens/home.dart
@@ -13,26 +13,88 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Column(
+      body: Stack(
         children: [
-          _searchBox(),
-          Expanded(
-            child: ListView(
+          Column(
+            children: [
+              _searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 0),
+                      margin: const EdgeInsets.only(
+                        top: 50,
+                        bottom: 20,
+                      ),
+                      child: const Text(
+                        "All ToDos",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    for (ToDo todoo in todosList)
+                      TodoItem(
+                        todo: todoo,
+                      ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
               children: [
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
-                  margin: const EdgeInsets.only(
-                    top: 50,
-                    bottom: 20,
-                  ),
-                  child: const Text(
-                    "All ToDos",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                      left: 20,
+                    ),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          hintText: "Add a new Todo Item",
+                          border: InputBorder.none),
+                    ),
                   ),
                 ),
-                for(ToDo todoo in todosList)
-                 TodoItem(todo: todoo,),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: tdBlue,
+                      minimumSize: const Size(60, 60),
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text(
+                      "+",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           )
